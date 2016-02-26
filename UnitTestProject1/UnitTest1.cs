1d01fd5549b185fbe20164c2c00e93bdfddd6e11
username <email@email.com>
2016-02-26 14:44:10 +0200

    ZigZag topcoder

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZigZag;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+      int[][] allSequences = new int[][] {
+        new int[] { 1, 7, 4, 9, 2, 5 },
+        new int[] { 1, 17, 5, 10, 13, 15, 10, 5, 16, 8 },
+        new int[] {44},
+        new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+        new int[]{ 70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32 },
+        new int[] { 374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
+600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
+67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
+477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
+249, 22, 176, 279, 23, 22, 617, 462, 459, 244 }
+      };
+      int[] allMax = new int[] { 6, 7, 1, 2, 8, 36 };
+
+      Program p = new Program();
+      int length = allSequences.Length;
+      for (int i = 0; i < length; i++)
+      {
+        int[] sequence = allSequences[i];
+
+        int[] actualSubSequence = null;
+        int actual = p.longestZigZag(sequence, out actualSubSequence);
+
+        int expected = allMax[i];
+
+        Assert.AreEqual(expected, actual);
+      }
+    }
+  }
+}
---
+++ ZigZag/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigZag
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public int longestZigZag(int[] sequence, out int[] subsequence)
+    {
+      subsequence = null;
+      int length = sequence.Length;
+
+      if (length == 1)
+      {
+        subsequence = new int[length];
+        Array.Copy(sequence, subsequence, length);
+        return 1;
+      }
+      if (length == 2)
+      {
+        subsequence = new int[length];
+        Array.Copy(sequence, subsequence, length);
+        return 2;
+      }
+
+      int[] solutions = Enumerable.Repeat(0, length).ToArray();
+      solutions[1] = sequence[1] > sequence[0] ? +2 : -2;
+      solutions[0] = solutions[1] > 0 ? -1 : 1;
+      int max = 2;
+      int solStart = 2;
+
+      for (int i = 2; i < length; i++)
+      {
+        for (int j = i - 1; j >= 0; j--)
+        {
+          if (sequence[i] != sequence[j] && Math.Sign(sequence[i] - sequence[j]) != Math.Sign(solutions[j]))
+          {
+            solutions[i] = Math.Abs(solutions[j]) + 1;
+            if (solutions[i] > max)
+            {
+              max = solutions[i];
+              solStart = i;
+            }
+            if (solutions[j] > 0)
+            {
+              solutions[i] *= -1;
+            }
+            break;
+          }
+        }
+      }
+
+      Queue<int> q = new Queue<int>();
+      q.Enqueue(sequence[solStart]);
+      int prev = solutions[solStart];
+      for (int i = solStart - 1; i >= 0; i--)
+      {
+        int curr = solutions[i];
+        if (Math.Sign(curr) != Math.Sign(prev))
+        {
+          q.Enqueue(sequence[i]);
+          prev = curr;
+        }
+      }
+      subsequence = q.Reverse().ToArray();
+
+      PrintArray(sequence);
+      PrintArray(solutions);
+
+      return max;
+    }
+
+    private void PrintArray(int[] arr)
+    {
+      Console.WriteLine(String.Join(", ", arr));
+    }
+  }
+}
